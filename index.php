e787e68b3aadf009a866a275d03e0b97b5ce25b8
emerset <emerset@gmail.com>
2015-05-01 14:25:11 -0400

    Add data into shoutit DB Validate data before adding

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include 'database.php';?>
 <?php 
 	// Create Select Query
-	$query = "SELECT * FROM shouts";
+	$query = "SELECT * FROM shouts ORDER BY id DESC";
 	$shouts = mysqli_query($con, $query);
 	
 ?>
@@ -25,6 +25,9 @@
 				</ul>
 			</div>
 			<div id="input">
+				<?php if (isset($_GET['error'])) : ?>
+					<div class="error"><?php echo $_GET['error'] ?></div>
+				<?php endif; ?>
 				<form method="post" action="process.php">
 					<input type="text" name="user" placeholder="Enter Your Name" />
 					<input type="text" name="message" placeholder="Enter A Message" />
